bc3ab709ff78fff8ea840491f53370cb000dcb8b
Jan <149733150+iBadassCheats@users.noreply.github.com>
2024-01-29 11:21:10 +0100

    Update Utils.cs

--- Utils.cs
+++ Utils.cs
@@ -24,25 +24,6 @@
             return null;
         }
 
-        [DllImport("user32.dll")]
-        static extern IntPtr GetForegroundWindow();
-
-        [DllImport("user32.dll")]
-        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
-
-        public string GetActiveWindowTitle()
-        {
-            const int nChars = 256;
-            StringBuilder Buff = new StringBuilder(nChars);
-            IntPtr handle = GetForegroundWindow();
-
-            if (GetWindowText(handle, Buff, nChars) > 0)
-            {
-                return Buff.ToString();
-            }
-            return null;
-        }
-
         public bool CheckWindow(Process process)
         {
             if (this.GetActiveWindowTitle() == process.MainWindowTitle || this.GetActiveWindowTitle() == this.title)
